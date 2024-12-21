4d886715504fcfbf329ed5861ed1d4f15e9b5efc
Patricia Torres <x00188069@mytudublin.ie>
2024-12-21 18:29:04 +0000

    Project initial commit

---
+++ style/myScript.js
@@ -0,0 +1,9 @@
+const toastTrigger = document.getElementById('sentMessage')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
